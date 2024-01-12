359f3e433be32dc0233ea9ab262b977ade82d379
eno <ryo.enomo@gmail.com>
2024-01-13 03:39:33 +0900

    [add]モデルとコントローラの追加
    ユーザー・施設・予約それぞれのモデルとコントローラ追加を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'reservations/new'
+  get 'reservations/create'
+  get 'reservations/index'
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/create'
+  get 'rooms/search'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/show'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
