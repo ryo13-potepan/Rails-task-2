ec1aef81f76980e914c5fb897032a45fa81e2a66
eno <ryo.enomo@gmail.com>
2024-01-26 01:45:56 +0900

    [add]ログイン画面/ドロップダウンメニュー項目のみ追加　jsは未実装

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   root 'tops#index'
   get 'search', to: 'tops#search', as: 'search'
 end
